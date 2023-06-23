baa07b99c97e91931ad84b12007fe0ba2e6f1fd3
Max Ivanov <waxivanov@gmail.com>
2023-06-23 11:48:19 +0300

    feat: get web3provider

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,22 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 
 const App: React.FC = () => {
+  const [provider, setProvider] = useState({});
+  const [signer, setSigner] = useState({});
+  const [signerAddress, setSignerAddress] = useState('');
+
+  useEffect(() => {
+    const onLoad = async () => {
+      const provider = await new ethers.providers.Web3Provider(window.ethereum);
+      setProvider(provider);
+    };
+
+    onLoad();
+  }, []);
+
   return <div>init</div>;
 };
 
